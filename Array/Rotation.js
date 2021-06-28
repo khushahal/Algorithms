eeebf757deab7e3d5d459398a442471ec33d4cdf
Suraj Bhushan Pandey <sbpandey186@gmail.com>
2021-06-28 20:19:47 +0530

    Added the data

--- Array/Rotation.js
+++ Array/Rotation.js
@@ -1,8 +1,1 @@
 
-/* left Rotate an array by the degree < the size of array */
-const _lrotate = (list = [], degree) => {
-  let result = list.splice(degree);
-  return [...list.reverse(), ...result.reverse()].reverse();
-};
-
-console.log(_lrotate([1, 2, 3, 4, 5, 6], 2));
