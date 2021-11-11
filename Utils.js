06628edb5171d6796aadbe081cae79bcb4310fc0
Suraj Bhushan Pandey <sbpandey186@gmail.com>
2021-11-11 17:11:35 +0530

    Added the Pollyfills Reducer

--- Utils.js
+++ Utils.js
@@ -4,5 +4,16 @@ const log = (message) => {
     console.log(message);
 }
 
+const _create2DArray = (rows, columns) => {
+    const _two2dArray = [];
+  
+    for (let i = 0; i < rows; ++i) _two2dArray.push(new Array(columns));
+  
+    return _two2dArray;
+};
 
-module.export = log;
+
+module.export = {
+    log,
+    _create2DArray
+};
